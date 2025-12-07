588c0e0b738df27eef6d2332af439cb53206c549
QATester <test@gmail.com>
2025-12-07 09:10:50 +0530

    Message

--- src/test/java/com/orangehrm/utils/DriverFactory.java
+++ src/test/java/com/orangehrm/utils/DriverFactory.java
@@ -4,6 +4,8 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DriverFactory {
@@ -13,8 +15,13 @@ public class DriverFactory {
 
     public static WebDriver getDriver() {
         if (driver == null) {
+        	ChromeOptions options = new ChromeOptions();
+        	options.addArguments("--headless=new");   // REQUIRED for GitHub Actions
+        	options.addArguments("--no-sandbox");
+        	options.addArguments("--disable-dev-shm-usage");
+        	options.addArguments("--window-size=1920,1080");
             WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
+            driver = new ChromeDriver(options);
             driver.manage().window().maximize();
             System.out.println("Browser maximized");
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(120));
